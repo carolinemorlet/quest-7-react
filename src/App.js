8d16d0bc9159c81cec5ac575899c0e3217810a22
Caroline Morlet <carolinemorlet@gmail.com>
2021-02-08 15:32:23 +0100

    quest 7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import QuoteCard from './components/QuoteCard';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  state = {
+    simpsons: [],
+  };
+
+  getSimpson = () => {
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      .then((resp) => resp.data)
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          simpsons: data[0],
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <QuoteCard simpson={this.state.simpsons} />
+        <button onClick={this.getSimpson}>Click</button>
+      </div>
+    );
+  }
 }
 
 export default App;
